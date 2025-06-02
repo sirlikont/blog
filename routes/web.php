dba5137c4f3292cbca476e8d860ad66716c277ac
Sirli Kont <sirlikont@gmail.com>
2025-06-02 12:15:17 +0300

    add route resource

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PublicController::class, 'index'])->name('home');
 Route::get('/about', [PublicController::class, 'about'])->name('about');
 
-Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/admin/posts/', [PostController::class, 'store'])->name('posts.store');
-Route::get('/admin/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+//Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
+//Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
+//Route::post('/admin/posts/', [PostController::class, 'store'])->name('posts.store');
+//Route::get('/admin/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+//Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+//Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+//Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+Route::resource('admin/posts', PostController::class);
 
